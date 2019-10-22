84261f1518d3a0d26d669af46a032701fc2673e1
Marian Radev <marianradiradev@gmail.com>
2019-10-22 12:09:58 +0300

    icons

--- app/src/main/java/com/rbx/ut/MainActivity.kt
+++ app/src/main/java/com/rbx/ut/MainActivity.kt
@@ -22,36 +22,11 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 		setSupportActionBar(toolbar)
 
-//		val courses = ArrayList<Course>()
-//		courses.add(
-//            Course(
-//                "Операционни системи за мобилни устройства",
-//                "CITB704",
-//                "314 I",
-//                "08:00 - 09:30"
-//            )
-//        )
-
 		val db = Room.databaseBuilder(
 			applicationContext,
 			AppDatabase::class.java, "production").allowMainThreadQueries().build()
 
-//        val db = Room.databaseBuilder(
-//            applicationContext,
-//            AppDatabase::class.java, "todo-list.db"
-//        ).build()
-
 		val courses: List<Course> = db.courseDao().getAllCourses()
-
-//		db.courseDao().insertAll(
-//			Course(
-//				"Генератори на компютърни игри",
-//				"CITB603",
-//				"702 II",
-//				"18:00 - 19:30"
-//		  )
-//		)
-
 
 		home_recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 		home_recyclerView.adapter = CourseAdapter(courses)
@@ -61,16 +36,16 @@ class MainActivity : AppCompatActivity() {
 		}
 	}
 
-	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		menuInflater.inflate(R.menu.menu_main, menu)
-		return true
-	}
+//	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		menuInflater.inflate(R.menu.menu_main, menu)
+//		return true
+//	}
 
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		return when (item.itemId) {
-			R.id.action_settings -> true
-			else -> super.onOptionsItemSelected(item)
-		}
-	}
+//	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//		return when (item.itemId) {
+//			R.id.action_settings -> true
+//			else -> super.onOptionsItemSelected(item)
+//		}
+//	}
 }
