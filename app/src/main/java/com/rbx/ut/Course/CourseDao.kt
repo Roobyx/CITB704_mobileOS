39da4ebed305a854a69ea97607b342401d49973c
Marian Radev <marianradiradev@gmail.com>
2019-10-23 08:26:38 +0300

    Finished MVP v0.1

--- app/src/main/java/com/rbx/ut/Course/CourseDao.kt
+++ app/src/main/java/com/rbx/ut/Course/CourseDao.kt
@@ -3,15 +3,21 @@ import androidx.room.*
 
 @Dao
 interface CourseDao {
+//	Main
+
 	@Query("SELECT * FROM CoursesTable")
 	fun getAllCourses():List<Course>
 
 	@Insert
 	fun insertAll(vararg courses:Course)
 
-    @Delete
-    fun delete(course: Course)
+	@Delete
+	fun delete(course: Course)
 
-    @Update
-    fun updateCourse(vararg courses: Course)
+	@Update
+	fun updateCourse(vararg courses: Course)
+
+//    Additional
+	@Query("DELETE FROM CoursesTable")
+	fun clearCoursesTable()
 }
